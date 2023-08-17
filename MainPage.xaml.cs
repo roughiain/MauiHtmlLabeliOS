453743b7fd1a58a23cdd9dc78b39cebcaeff8bf3
Iain Rough <roughiain@gmail.com>
2023-08-17 08:44:34 +0200

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiHtmlLabelPlatformSpike;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+        Label label = new Label
+        {
+            Text = "This is <strong style=\"color:red\">HTML</strong> text. from c#",
+            TextType = TextType.Html
+        };
+		TheVerticalStackLayout.Children.Add(label);
+    }
+
+}
+
+
